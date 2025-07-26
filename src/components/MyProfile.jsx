308e8a34869c3dc075bfc5b3748a22df834798aa
yatinsingh2007 <yatin4591@gmail.com>
2025-07-26 12:45:31 +0530

    profile fixes

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const MyProfile = () => {
   const { theme } = useContext(ThemeContext);
   const navigate = useNavigate()
-  let userData = JSON.parse(localStorage.getItem("userData"))?.user_data?.[0]
+  let userData = JSON.parse(localStorage.userData)?.user
   if(!userData) {
     let userData = {}
     userData.user = {
