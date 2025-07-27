b448428d5ca967e552460c5d8199ba6294b7345c
yatinsingh2007 <yatin4591@gmail.com>
2025-07-27 23:49:48 +0530

    display allign fex

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -89,7 +89,8 @@ const MyProfile = () => {
             })}
           </div>
           <div className="flex flex-row-reverse mt-6">
-            <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+            <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400
+            mr-12"
               onClick={
                 async (e) => {
                   e.preventDefault();
